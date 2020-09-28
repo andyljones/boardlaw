2bab379e3162956a317b76826b4206d6202d480b
Andrew Jones <andyjones.ed@gmail.com>
2020-09-28 13:12:18 +0000

    Adds an init file

---
+++ scalinglaws/__init__.py
@@ -0,0 +1,5 @@
+"""Attack:
+* Get a simple Go variant running on the GPU. Test against OpenSpiel.
+* Get a simple AlphaZero variant running on the GPU. Test against OpenSpiel.
+* Get some scaling laws!
+"""
