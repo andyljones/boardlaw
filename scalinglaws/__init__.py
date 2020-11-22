cf808959f7e6cace6bd6d275ca620169db13e813
Andrew Jones <andyjones.ed@gmail.com>
2020-11-22 14:43:24 +0000

    Lowers noise eps, fixes a bunch of other stuff

--- scalinglaws/__init__.py
+++ scalinglaws/__init__.py
@@ -41,13 +41,18 @@ def chunk_stats(chunk, n_new):
         r = t.rewards[1:][t.terminal[1:]]
         stats.mean('progress/terminal-1-corr', ((v - v.mean())*(r - r.mean())).mean()/(v.var()*r.var())**.5)
 
+def rel_entropy(logits, valid):
+    zeros = torch.zeros_like(logits)
+    logits = logits.where(valid, zeros)
+    probs = logits.exp().where(valid, zeros)
+    return (-(logits*probs).sum(-1).mean(), torch.log(valid.sum(-1).float()).mean())
 
 def optimize(network, opt, batch):
     w, d0, t = batch.worlds, batch.decisions, batch.transitions
     d = network(w, value=True)
 
     zeros = torch.zeros_like(d.logits)
-    policy_loss = -(d0.logits.exp()*d.logits).where(w.valid, zeros).sum(axis=1).mean()
+    policy_loss = -(d0.logits.exp()*d.logits).where(w.valid, zeros).sum(axis=-1).mean()
 
     terminal = torch.stack([t.terminal for _ in range(w.n_seats)], -1)
     target_value = learning.reward_to_go(t.rewards, d0.v, terminal, terminal, gamma=1)
@@ -66,10 +71,10 @@ def optimize(network, opt, batch):
         stats.mean('loss/value', value_loss)
         stats.mean('loss/policy', policy_loss)
         stats.mean('progress/resid-var', (target_value - d.v).pow(2).mean(), target_value.pow(2).mean())
+        stats.mean('progress/kl-div', -(d0.logits - d.logits).where(w.valid, zeros).mean())
 
-        logits = d.logits.where(w.valid, zeros)
-        probs = d.logits.exp().where(w.valid, zeros)
-        stats.mean('progress/rel-entropy', -(logits*probs).sum(-1).mean(), torch.log(w.valid.sum(-1).float()).mean())
+        stats.mean('rel-entropy/policy', *rel_entropy(d.logits, w.valid)) 
+        stats.mean('rel-entropy/targets', *rel_entropy(d0.logits, w.valid))
 
         stats.mean('v-target/mean', target_value.mean())
         stats.mean('v-target/std', target_value.std())
@@ -91,7 +96,7 @@ def run():
     opt = torch.optim.Adam(network.parameters(), lr=1e-3, amsgrad=True)
 
     idiot = validation.MonteCarloAgent(1)
-    evaluator = analysis.Evaluator(worlds[:1], [idiot], n_trajs=32, throttle=60)
+    evaluator = analysis.Evaluator(worlds[:1], [idiot], n_trajs=32, throttle=300)
 
     run_name = paths.timestamp('az-test')
     compositor = widgets.Compositor()
--- scalinglaws/mcts.py
+++ scalinglaws/mcts.py
@@ -111,7 +111,7 @@ def dirichlet_noise(logits, valid, alpha=None, eps=.25):
 
 class MCTS:
 
-    def __init__(self, world, n_nodes, c_puct=2.5, noise_eps=.25):
+    def __init__(self, world, n_nodes, c_puct=2.5, noise_eps=.05):
         self.device = world.device
         self.n_envs = world.n_envs
         self.n_nodes = n_nodes
