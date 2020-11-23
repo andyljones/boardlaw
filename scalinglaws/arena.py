3bf9c0c2022457e6544058e101ebd4c18b7630c4
Andrew Jones <andyjones.ed@gmail.com>
2020-11-23 20:22:37 +0000

    Replaces the serial arena with a parallel version

--- scalinglaws/arena.py
+++ scalinglaws/arena.py
@@ -1,10 +1,11 @@
+import numpy as np
 import pandas as pd
 import pickle
 import torch
 from rebar import paths, storing, arrdict
 from logging import getLogger
 from . import analysis
-from itertools import permutations
+from itertools import combinations, permutations
 
 log = getLogger(__name__)
 
@@ -26,11 +27,8 @@ def latest_agent(agentfunc, run_name):
     sd = storing.load_latest(run_name)
     return assemble_agent(agentfunc, sd)
 
-def run(worldfunc, agentfunc, run_name):
-    agents = periodic_agents(agentfunc, run_name)
-    agents['latest'] = latest_agent(agentfunc, run_name)
-
-    worlds = worldfunc(n_envs=32)
+def league(worldfunc, agents, n_copies=32):
+    worlds = worldfunc(n_envs=n_copies)
     scores = arrdict.arrdict()
     for first, second in permutations(agents, 2):
         log.info(f'Evaluating {first} v {second}')
@@ -45,5 +43,43 @@ def run(worldfunc, agentfunc, run_name):
         scores[first, second] = (rewards/terminals)[0]
 
     return pd.Series(scores).apply(float).unstack()
+
+def parallel_league(worldfunc, agents, n_copies=32):
+    n_agents = len(agents)
+
+    idxs = np.arange(n_copies*n_agents*n_agents)
+    fstmask, sndmask, _ = np.unravel_index(idxs, (n_agents, n_agents, n_copies))
+
+    worlds = worldfunc(len(idxs))
+    fstmask = torch.as_tensor(fstmask, device=worlds.device) 
+    sndmask = torch.as_tensor(sndmask, device=worlds.device)
+
+    terminations = torch.zeros((worlds.n_envs), device=worlds.device)
+    rewards = torch.zeros((worlds.n_envs, 2), device=worlds.device)
+    while True:
+        log.info(f'Stepped; {(terminations >= 1).float().mean():.0%} have terminated')
+        for (i, first) in enumerate(agents):
+            mask = (fstmask == i) & (terminations < 1)
+            if mask.any():
+                decisions = agents[first](worlds[mask])
+                worlds[mask], transitions = worlds[mask].step(decisions.actions)
+                rewards[mask & (terminations < 1), 0] += (transitions.rewards[terminations[mask] < 1, 0] == 1)
+                terminations[mask] += transitions.terminal
+        
+        for (j, second) in enumerate(agents):
+            mask = (sndmask == j) & (terminations < 1)
+            if mask.any():
+                decisions = agents[second](worlds[mask])
+                worlds[mask], transitions = worlds[mask].step(decisions.actions)
+                rewards[mask & (terminations < 1), 1] += (transitions.rewards[terminations[mask] < 1, 1] == 1)
+                terminations[mask] += transitions.terminal
+        
+        if (terminations >= 1).all():
+            break
+
+
+def run(worldfunc, agentfunc, run_name):
+    agents = periodic_agents(agentfunc, run_name)
+    agents['latest'] = latest_agent(agentfunc, run_name)
 
 
