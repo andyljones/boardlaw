fd1775a28828dd1b171583320b9b8403a278cbc4
Andrew Jones <andyjones.ed@gmail.com>
2020-11-27 21:57:57 +0000

    Improves the arena plotting

--- scalinglaws/arena/plot.py
+++ scalinglaws/arena/plot.py
@@ -8,8 +8,8 @@ def plot(df):
         ax = sns.heatmap(df, cmap='RdBu', annot=True, vmin=0, vmax=1, annot_kws={'fontsize': 'large'}, cbar=False, square=True)
         return ax
 
-def plot_black():
-    df = (database.stored()
+def plot_black(run_name):
+    df = (database.stored(run_name)
             .assign(black_win=lambda df: df.black_reward == 1)
             .groupby(['black_name', 'white_name']).black_win.mean()
             .unstack())
@@ -17,8 +17,8 @@ def plot_black():
     ax.set_xlabel('white')
     ax.set_ylabel('black')
 
-def plot_symmetric():
-    stats = (database.stored()
+def plot_symmetric(run_name):
+    stats = (database.stored(run_name)
                 .assign(
                     wins=lambda df: df.black_reward == 1,
                     games=lambda df: pd.Series(1, df.index))
