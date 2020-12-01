5dfc12e7f7ea91ecd87463ac65a75d0aedf5d92d
Andrew Jones <andyjones.ed@gmail.com>
2020-12-01 23:56:35 +0000

    SymPy cleanup

--- scalinglaws/arena/vb.py
+++ scalinglaws/arena/vb.py
@@ -5,26 +5,25 @@ import sympy as sym
 σ0 = 1
 
 def winrate(μ, Σ):
-    # from sympy import *
-    # from sympy import stats
-    # from math import inf
-    #
     # μ = MatrixSymbol('μ', 2, 1)
     # Λ = MatrixSymbol('Λ', 2, 2)
     # d, m = symbols('d m')
     # I = Matrix([[1/2], [1/2]])
     # D = Matrix([[1/2], [-1/2]])
-    #
+
     # integrand = trace(Matrix(-1/2*(m*I + d*D - μ).T @ Λ @ (m*I + d*D - μ)))
-    # integrand = poly(integrand, m)
-    #
+    # integrand = integrand.subs(Λ[0, 1], Λ[1, 0])
+    # integrand = poly(integrand, m).coeffs()
+
     # a, b, c = symbols('a b c')
-    # pattern = poly(-a*(m - b)**2 + c, m)
-    #
-    # [(a, b, c)] = solve([Eq(l, r) for l, r in zip(pattern.coeffs(), integrand.coeffs())], (a, b, c))
-    #
-    # c = simplify(factor(c))
+    # gaussian_integrand = poly(-a*(m - b)**2 + c, m).coeffs()
+
+    # [(a, b, c)] = solve([Eq(l, r) for l, r in zip(gaussian_integrand, integrand)], (a, b, c))
+
+    # a = simplify(a)
+    # c = simplify(factor(c), rational=True)
     # integral = E**c * sqrt(pi/a)
+
 
     pass
 
@@ -38,5 +37,7 @@ def joint_prob(n, w, μ, Σ):
     # from sympy.stats import E, Normal
     # s, μ, μ0, σ, σ0 = symbols('s μ μ_0 σ σ_0')
     # s = Normal('s', μ, σ)
-    # 1/(2*σ0)*E((s - μ0)**2)
-    prior = 1/(2*σ0)*((μ - μ0)**2 + Σ**2)
+    # 1/(2*σ0)*E(-(s - μ0)**2)
+    prior = -1/(2*σ0)*((μ - μ0)**2 + Σ**2)
+
+    return likelihood.sum() + prior.sum()
