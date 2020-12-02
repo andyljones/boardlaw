6a32fdc6188d6644c2591ada6255a036a8d6355f
Andrew Jones <andyjones.ed@gmail.com>
2020-12-02 13:35:56 +0000

    Gets a basic routine going. Doesn't do very well

--- scalinglaws/arena/vb.py
+++ scalinglaws/arena/vb.py
@@ -61,27 +61,63 @@ class LUT:
     def plot(self):
         Y, X = np.meshgrid(self.μ_range, self.σ_range)
         (t, b), (l, r) = μ_lims, σ_lims
-        plt.imshow(np.exp(self.expectation), extent=(l, r, b, t), vmin=0, vmax=1, cmap='RdBu')
+        plt.imshow(np.exp(self.expectation), extent=(l, r, b, t), vmin=0, vmax=1, cmap='RdBu', aspect='auto')
         plt.colorbar()
 
-def likelihood(n, w, μ, Σ):
-    if not hasattr(likelihood, '_lut'):
-        likelihood._lut = LUT(lambda d: -np.log(1 + np.exp(-d)), n.shape[0])
-    lut = likelihood._lut
+def expected_log_likelihood(n, w, μ, Σ):
+    self = expected_log_likelihood
+    if not hasattr(self, '_lut'):
+        self._lut = LUT(lambda d: -np.log(1 + np.exp(-d)), n.shape[0])
+    lut = self._lut
 
     return w*lut.winrates(μ, Σ) + (n - w)*lut.winrates(-μ, Σ)
 
-
-def joint_prob(n, w, μ, Σ):
+def cross_entropy(n, w, μ, Σ):
 
     # Proof:
     # from sympy.stats import E, Normal
     # s, μ, μ0, σ, σ0 = symbols('s μ μ_0 σ σ_0')
     # s = Normal('s', μ, σ)
     # 1/(2*σ0)*E(-(s - μ0)**2)
-    prior = -1/(2*σ0)*((μ - μ0)**2 + Σ**2)
+    expected_prior = -1/(2*σ0)*((μ - μ0)**2 + Σ**2)
 
-    return likelihood(n, w, μ, Σ).sum() + prior.sum()
+    return -expected_log_likelihood(n, w, μ, Σ).sum() - expected_prior.sum()
+
+def entropy(Σ):
+    _, logdet = np.linalg.slogdet(2*np.pi*np.e*Σ)
+    return 1/2*logdet
+
+def elbo(n, w, μ, Σ):
+    return -cross_entropy(n, w, μ, Σ) + entropy(Σ)
+
+def solve(n, w):
+    N = n.shape[0]
+
+    def pack(μ, Σ):
+        return np.concatenate([μ, Σ.flatten()])
+
+    def unpack(x):
+        μ = x[:N]
+        Σ = x[N:].reshape(N, N)
+        Σsym = (Σ + Σ.T)/2
+
+        λ, v = np.linalg.eigh(Σsym)
+        Σproj = v @ np.diag(λ.clip(1e-3, None)) @ v.T 
+        
+        return μ, Σproj
+
+    def f(x):
+        μ, Σ = unpack(x)
+        return -elbo(n, w, μ, Σ)
+
+    μ0 = np.zeros((N,))
+    Σ0 = np.eye(N)
+    x0 = pack(μ0, Σ0)
+
+    soln = sp.optimize.minimize(f, x0)
+    μf, Σf = unpack(soln.x)
+
+    return μf, Σf
 
 def test():
     N = 5
@@ -93,7 +129,4 @@ def test():
     d = s[:, None] - s[None, :]
     w = sp.stats.binom(n, 1/(1 + np.exp(-d))).rvs()
 
-    μ = np.zeros((N,))
-    Σ = np.eye(N)
-    
-    joint_prob(n, w, μ, Σ)
+    μf, Σf = solve(n, w)
