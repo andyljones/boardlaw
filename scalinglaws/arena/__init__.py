c1de8a448ebc305278c87c3136e1b1f96774d45a
Andrew Jones <andyjones.ed@gmail.com>
2020-12-08 22:58:19 +0000

    Adds latest result accumulation

--- scalinglaws/arena/__init__.py
+++ scalinglaws/arena/__init__.py
@@ -93,6 +93,8 @@ def step_mohex(run_name, worlds, agents):
     log.info(f'Storing. {wins} wins in {games} games for {list(agents)[0]} ')
     database.store(run_name, results)
 
+_latest_matchup = None
+_latest_results = []
 def step_latest(run_name, worlds, agents):
     n, w = database.symmetric_pandas(run_name, agents)
     log.info(f'Loaded {int(n.sum().sum())} games')
@@ -105,18 +107,25 @@ def step_latest(run_name, worlds, agents):
     log.info('Playing ' + ' v. '.join(agents))
     results = matchups.evaluate(worlds.periodic, agents)
 
-    for r in results:
+    global _latest_matchup, _latest_results
+    if _latest_matchup != matchup:
+        _latest_matchup = matchup
+        _latest_results = []
+    _latest_results.extend(results)
+
+    log.info(f'Got {len(_latest_results)} results to accumulate')
+    for r in _latest_results:
         w.loc[r.names[0], r.names[1]] += r.wins[0]
         w.loc[r.names[1], r.names[0]] += r.wins[1]
         n.loc[r.names[0], r.names[1]] += r.games
+        n.loc[r.names[1], r.names[0]] += r.games
     
-    wins, games = int(results[0].wins[0] + results[1].wins[1]), int(sum(r.games for r in results))
+    wins, games = int(w.loc[matchup[0], matchup[1]]), int(n.loc[matchup[0], matchup[1]])
     log.info(f'Fitting posterior. {wins} wins for {list(agents)[0]} in {games} games')
     soln = activelo.solve(n.values, w.values)
     log.info(f'Fitted posterior, {(soln.σd**2).mean()**.5:.2f}σd over {n.shape[0]} agents')
     μ = pd.Series(soln.μ, n.index)
     μ = μ.loc[latest] - μ[μ.index.str.endswith('periodic')].iloc[0]
-    breakpoint()
     log.info(f'eElo for {latest} is approximately {μ:.2f} v. the first agent')
     stats.mean('elo', μ)
 
