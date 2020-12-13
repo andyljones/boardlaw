69c1fe732309199fa001c439e675e87a184a3c97
Andrew Jones <andyjones.ed@gmail.com>
2020-12-13 14:30:04 +0000

    Tests up the initial token placement in hex

--- scalinglaws/hex/cpp/kernels.cu
+++ scalinglaws/hex/cpp/kernels.cu
@@ -6,8 +6,21 @@
 
 const uint BLOCK = 8;
 
+enum {
+    EMPTY,
+    BLACK,
+    BLACK_WIN,
+    TOP,
+    BOT, 
+    WHITE,
+    WHITE_WIN,
+    LEFT,
+    RIGHT
+};
+
+
 __global__ void step_kernel(
-    I3D::PTA board, I1D::PTA seats, I1D::PTA actions, I1D::PTA results) {
+    I3D::PTA board, I1D::PTA seats, I1D::PTA actions, F2D::PTA results) {
 
     const uint B = board.size(0);
     const uint S = board.size(1);
@@ -15,23 +28,59 @@ __global__ void step_kernel(
 
     if (b >= B) return;
 
-    int row = actions[b] / S;
-    int col = actions[b] % S;
+    const int seat = seats[b]; 
 
-    board[b][row][col] = 1;
 
-    results[b] = 1;
+    // Swap rows and cols if we're playing white
+    const int action = actions[b];
+    int row, col;
+    if (seat == 0) { row = action / S, col = action % S; }
+    else           { row = action % S, col = action / S; }
 
+    // Set up the adjacency indicator
+    __shared__ bool adj[9];
+    for (int a=0; a<9; a++) {
+        adj[a] = false;
+    }
+
+    // Populate the adjacency indicator
+    for (int i=-1; i<=1; i++) {
+        for (int j=-1; j<=1; j++) {
+            int r = row + i;
+            int c = col + j;
+            if      (r <  0) { adj[TOP] = true; }
+            else if (r >= S) { adj[BOT] = true; }
+            else if (c <  0) { adj[LEFT] = true; }
+            else if (c >= S) { adj[RIGHT] = true; }
+            else { adj[board[b][r][c]] = true; }
+        }
+    }
+
+    // Use the adjacency to decide what the new cell should be
+    char new_cell;
+    if (seat) {
+        if (adj[LEFT] && adj[RIGHT]) { new_cell = WHITE_WIN; results[b][1] = 1.f; } 
+        else if (adj[LEFT]) { new_cell = LEFT; } 
+        else if (adj[RIGHT]) { new_cell = RIGHT; } 
+        else { new_cell = WHITE; }
+    } else {
+        if (adj[TOP] && adj[BOT]) { new_cell = BLACK_WIN; results[b][0] = 1.f; } 
+        else if (adj[TOP]) { new_cell = TOP; } 
+        else if (adj[BOT]) { new_cell = BOT; } 
+        else { new_cell = BLACK; }
+    }
+
+    board[b][row][col] = new_cell;
 }
 
 __host__ TT step(TT board, TT seats, TT actions) {
     const uint B = board.size(0);
 
-    TT results = at::zeros_like(actions);
+    TT results = board.new_zeros({B, 2}, at::kFloat);
 
     const uint n_blocks = (B + BLOCK - 1)/BLOCK;
     step_kernel<<<{n_blocks}, {BLOCK}>>>(
-        I3D(board).pta(), I1D(seats).pta(), I1D(actions).pta(), I1D(results).pta());
+        I3D(board).pta(), I1D(seats).pta(), I1D(actions).pta(), F2D(results).pta());
 
     return results;
 }
--- scalinglaws/hex/cuda.py
+++ scalinglaws/hex/cuda.py
@@ -1,5 +1,6 @@
 import torch
 import torch.cuda
+import torch.testing
 from .. import cuda
 
 loaded = cuda.load(__package__)
@@ -7,11 +8,46 @@ for k in dir(loaded):
     if not k.startswith('__'):
         globals()[k] = getattr(loaded, k)
 
-def test():
-    board = torch.zeros((1, 3, 3)).int().cuda()
-    seats = torch.zeros((1,)).int().cuda()
-    actions = torch.tensor([1]).int().cuda()
+def empty_board():
+    return torch.tensor([[
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]]]).int().cuda()
 
-    results = loaded.step(board, seats, actions)
+def move_board(*moves):
+    board = empty_board()
+    for i, j, v in moves:
+        board[:, i, j] = v
+    return board
 
-    print(board[0])
+def test_move(seat, action, initial, expected):
+    seats = torch.tensor([seat]).int().cuda()
+    actions = torch.tensor([action]).int().cuda()
+    
+    result = loaded.step(initial, seats, actions)
+    torch.testing.assert_allclose(initial, expected)
+    return result
+
+def test_single_moves():
+    # Black
+    test_move(0, 4, empty_board(), move_board((1, 1, 1))) # black
+    test_move(0, 0, empty_board(), move_board((0, 0, 3))) # top 
+    test_move(0, 8, empty_board(), move_board((2, 2, 4))) # bot
+
+    # White
+    test_move(1, 4, empty_board(), move_board((1, 1, 5))) # white
+    test_move(1, 0, empty_board(), move_board((0, 0, 7))) # left
+    test_move(1, 8, empty_board(), move_board((2, 2, 8))) # right
+
+def test_wins():
+    # Black win
+    result = test_move(0, 4, 
+        move_board((0, 1, 3), (2, 1, 4)),
+        move_board((0, 1, 3), (2, 1, 4), (1, 1, 2)))
+    torch.testing.assert_allclose(result, torch.tensor([1., 0.]).cuda())
+
+    # White win
+    result = test_move(1, 4, 
+        move_board((1, 0, 7), (1, 2, 8)),
+        move_board((1, 0, 7), (1, 2, 8), (1, 1, 6)))
+    torch.testing.assert_allclose(result, torch.tensor([0., 1.]).cuda())
