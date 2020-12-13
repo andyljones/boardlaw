826c189c1acbd5aa84d8d88b74079d105632c5a8
Andrew Jones <andyjones.ed@gmail.com>
2020-12-13 23:05:40 +0000

    Isolates the flooding bug

--- scalinglaws/hex/__init__.py
+++ scalinglaws/hex/__init__.py
@@ -220,18 +220,16 @@ def test_bug():
         [4, 2, 0]], device=worlds.device))
 
 def test_bug_2():
-    worlds = Hex.initial(n_envs=1, boardsize=11)
-    worlds.board[:] = torch.tensor([[5, 3, 3, 6, 6, 6, 6, 6, 3, 6, 6],
-            [0, 6, 6, 6, 6, 4, 6, 6, 6, 6, 6],
-            [1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-            [1, 4, 4, 4, 5, 4, 2, 2, 2, 4, 4],
-            [5, 5, 4, 4, 5, 4, 2, 2, 4, 4, 6],
-            [5, 5, 5, 5, 4, 4, 4, 2, 4, 4, 6],
-            [5, 4, 4, 5, 5, 4, 2, 2, 4, 6, 6],
-            [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-            [4, 5, 5, 4, 6, 6, 6, 4, 6, 6, 6],
-            [5, 4, 4, 6, 4, 6, 6, 6, 6, 4, 4],
-            [5, 5, 4, 6, 4, 6, 6, 4, 4, 2, 4]], device=worlds.device, dtype=torch.uint8)
+    worlds = Hex.initial(n_envs=1, boardsize=3)
+    worlds.board[:] = torch.tensor([
+        [0, 6, 6],
+        [1, 1, 1],
+        [0, 2, 0]], device=worlds.device, dtype=torch.uint8)
     worlds.seats[:] = 0
 
-    worlds, transitions = worlds.step(torch.tensor([11]), device=worlds.device)
+    worlds, transitions = worlds.step(torch.tensor([6], device=worlds.device))
+
+    torch.testing.assert_allclose(worlds.board[0], torch.tensor([
+        [0, 6, 6],
+        [4, 4, 4],
+        [4, 2, 0]], device=worlds.device))
