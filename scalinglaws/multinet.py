abfd6edcf6dd4160a7ba1daea72d26d3df301a00
Andrew Jones <andyjones.ed@gmail.com>
2020-12-16 20:52:43 +0000

    Attempts to get streaming working

--- scalinglaws/multinet.py
+++ scalinglaws/multinet.py
@@ -18,7 +18,22 @@ class Naive(nn.Module):
             y[idxs == i] = l(x[idxs == i])
         return F.relu(y)
 
-def benchmark(features=128, layers=1, models=1, envs=8192, T=128, device='cuda:1'):
+class Streamed(nn.Module):
+
+    def __init__(self, in_features, out_features, n_models):
+        super().__init__()
+        self.layers = nn.ModuleList([nn.Linear(in_features, out_features) for _ in range(n_models)])
+        self.out_features = out_features
+        self.streams = [torch.cuda.Stream() for _ in range(n_models)]
+
+    def forward(self, x, idxs):
+        y = x.new_empty((*x.shape[:-1], self.out_features))
+        for i, l in enumerate(self.layers):
+            with torch.cuda.stream(self.streams[i]):
+                y[idxs == i] = l(x[idxs == i])
+        return F.relu(y)
+
+def benchmark(cls, features=128, layers=1, models=1, envs=8192, T=128, device='cuda:1'):
     assert envs % models == 0
 
     x = torch.zeros((envs, features), device=device)
@@ -26,7 +41,7 @@ def benchmark(features=128, layers=1, models=1, envs=8192, T=128, device='cuda:1
 
     model = []
     for _ in range(layers):
-        model.extend([Naive(features, features, models)])
+        model.extend([cls(features, features, models)])
     model = recurrence.Sequential(*model).to(device)
 
     with aljpy.timer() as timer:
@@ -37,4 +52,7 @@ def benchmark(features=128, layers=1, models=1, envs=8192, T=128, device='cuda:1
     return 1e6*timer.time()/(T*envs)
 
 def profile(**kwargs):
-    return pd.Series({m: benchmark(**kwargs, models=m) for m in [1, 2, 4, 8, 16, 32, 64]})
+    results = {}
+    for cls in [Naive, Streamed]:
+        results[cls.__name__] = pd.Series({m: benchmark(cls, **kwargs, models=m) for m in [1, 2, 4, 8, 16, 32, 64]})
+    return pd.concat(results, 1)
