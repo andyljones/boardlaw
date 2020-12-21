18a9e3b7c4ec1c3323fbffcb90b51eb2f93a9dc6
Andrew Jones <andyjones.ed@gmail.com>
2020-12-21 10:31:09 +0000

    Switches to using a simple form of the Fisher information for sensitivities

--- activelo/activelo/suggestions.py
+++ activelo/activelo/suggestions.py
@@ -19,28 +19,17 @@ def sensitivities(Σ):
     # Change in trace if we conditioned I-J out
     return ((Σ[:, None, :] - Σ[None, :, :])**2).sum(-1)
 
-def posterior_var_ratio(μd, σd, G):
-    ϕ = lambda d: 1/(1 + np.exp(-d))
-    diϕ = lambda r: 1/r + 1/(1 - r)
-    dϕ = lambda d: -np.exp(-d)/(1 + np.exp(-d))**2
+def improvement(soln):
+    e = np.exp(-soln.μd)
+    # This is the Fisher info for one game
+    fisher_info = 1/(1/e + 2 + e)
+    return fisher_info*sensitivities(soln.Σ)
 
-    num = solvers.σ0**2 * dϕ(μd)**2 * σd**2 
-    denom = solvers.σ0**2 + dϕ(μd)**2 *σd**2 * G
-
-    posterior_var = diϕ(ϕ(μd))**2 * num/denom
-
-    return safe_divide(posterior_var, σd**2)
-
-def improvement(soln, G):
+def suggest(soln):
     if isinstance(soln.μ, pd.Series):
-        return common.pandify(improvement(common.numpyify(soln), G), soln.μ.index)
-    return safe_divide(sensitivities(soln.Σ), posterior_var_ratio(soln.μd, soln.σd, G))
-
-def suggest(soln, G):
-    if isinstance(soln.μ, pd.Series):
-        row, col = suggest(common.numpyify(soln), G)
+        row, col = suggest(common.numpyify(soln))
         return soln.μ.index[row], soln.μ.index[col]
-    idx = improvement(soln, G).argmax()
+    idx = improvement(soln).argmax()
     return np.unravel_index(idx, soln.μd.shape) 
 
 def test_sensitivities():
@@ -58,7 +47,4 @@ def test_improvement():
     first = solver(n, w)
     second = solver(2*n, 2*n)
 
-    expected = second.σd[0, 1]/first.σd[0, 1]
-    actual = improvement(first, k)
-
-    torch.testing.assert_allclose(expected, actual)
+    actual = improvement(first)
