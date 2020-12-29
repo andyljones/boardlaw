5241dc6b4846e65eaa56d45ed85a84b7e84c1d47
Andrew Jones <andyjones.ed@gmail.com>
2020-12-29 19:40:59 +0000

    Adds json support to pavlov

--- pavlov/json.py
+++ pavlov/json.py
@@ -3,14 +3,26 @@ from . import runs, files
 from portalocker import RLock
 from contextlib import contextmanager
 
+def path(run, prefix):
+    return files.path(run, f'{prefix}.json')
+
+@contextmanager
+def lock(run, prefix):
+    lockpath = path(run, prefix).with_suffix('.json.lock')
+    with RLock(lockpath):
+        yield
+
 def new(run, prefix):
-    pass
+    with lock(run, prefix):
+        files.new_file(run, f'{prefix}.json')
 
 @contextmanager
 def update(run, prefix):
-    path = files.path(run, f'{prefix}.json')
-    lockpath = path.with_suffix('.json.lock')
-    with RLock(lockpath):
-        contents = path.read_text()
+    with lock(run, prefix):
+        contents = path(run, prefix).read_text()
         yield contents
         path.write_text(contents)
+
+def read(run, prefix):
+    with lock(run, prefix):
+        return json.loads(path(run, prefix).read_text())
