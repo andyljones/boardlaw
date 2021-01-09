fd88e6e5145dcd74d5fa08e629c0a90aa6cda31d
Andrew Jones <andyjones.ed@gmail.com>
2021-01-09 17:52:48 +0000

    Swaps to randomized sample generation for the victory test

--- experiments/attn/victory.py
+++ experiments/attn/victory.py
@@ -7,9 +7,9 @@ from . import common
 from tqdm.auto import tqdm
 from boardlaw.hex import Hex
 from torch.nn import functional as F
+from boardlaw.main import worldfunc, agentfunc
 
 def gamegen(n_envs=1024):
-    from boardlaw.main import worldfunc, agentfunc
     from pavlov import storage
 
     worlds = worldfunc(n_envs)
@@ -20,50 +20,41 @@ def gamegen(n_envs=1024):
     agent.load_state_dict(sd['agent'])
 
     while True:
-        with torch.no_grad():
-            decisions = agent(worlds, eval=False)
-        new_worlds, transitions = worlds.step(decisions.actions)
+        # with torch.no_grad():
+        #     decisions = agent(worlds, eval=False)
+        actions = torch.distributions.Categorical(probs=worlds.valid.float()).sample()
+        new_worlds, transitions = worlds.step(actions)
 
         if transitions.terminal.any():
             yield arrdict.arrdict(
-                worlds=arrdict.to_dicts(worlds[transitions.terminal]),
-                actions=decisions.actions[transitions.terminal],
+                worlds=worlds[transitions.terminal],
+                actions=actions[transitions.terminal],
                 rewards=transitions.rewards[transitions.terminal])
 
         worlds = new_worlds
 
-        
-def save_boards(n_envs=32*1024, total=1024*1024):
-    boards = []
-    with tqdm(total=total) as pbar:
-        for b in gamegen(n_envs):
-            boards.append(b)
-            count = sum(b.actions.size(0) for b in boards)
-            pbar.update(count - pbar.n)
-            if count >= total:
-                break
-
-    boards = arrdict.cat(boards)
-    Path('output/terminal-boards.pkl').write_bytes(pickle.dumps(boards))
-
-def load_boards():
-    return pickle.loads(Path('output/terminal-boards.pkl').read_bytes())
+def batchgen(B, n_envs):
+    buffer = []
+    for x in gamegen(n_envs):
+        buffer.append(x)
+        n = sum(b.actions.size(0) for b in buffer)
+        if n >= B:
+            buffer = arrdict.cat(buffer)
+            yield buffer[:B]
+            buffer = [buffer[B:]]
 
 def run(D=32, B=8*1024, T=5000, device='cuda'):
-    boards = arrdict.from_dicts(load_boards()).to(device)
-    worlds = Hex(boards.worlds)
 
-    n_boards, boardsize, _ = worlds.board.shape
+    boardsize = worldfunc(1).board.shape[-1]
     model = common.FCModel(common.PosActions, boardsize, D).to(device)
 
     opt = torch.optim.Adam(model.parameters(), lr=1e-2)
 
     with tqdm(total=T) as pbar:
-        for t in range(T):
-            idxs = torch.randint(0, n_boards, size=(B,), device=device)
-            outputs = model(worlds[idxs].obs)
+        for t, b in zip(range(T), batchgen(B, B)):
+            outputs = model(b.worlds.obs)
 
-            loss = F.nll_loss(outputs.reshape(B, -1), boards.actions[idxs])
+            loss = F.nll_loss(outputs.reshape(B, -1), b.actions)
 
             opt.zero_grad()
             loss.backward()
