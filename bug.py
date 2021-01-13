0458e9afb9e61f47d33b72b2667bb331e9fda7a0
Andrew Jones <andyjones.ed@gmail.com>
2021-01-13 15:53:13 +0000

    Even more minimal bug

--- bug.py
+++ bug.py
@@ -1,61 +1,53 @@
+import os
+os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+
 import numpy as np
 import torch
 from torch import nn
 
 class Model(nn.Module):
 
-    def __init__(self, width=16, depth=16):
+    def __init__(self, channels=16):
         super().__init__()
 
-        layers = [nn.Conv2d(14, width, 3, 1, 1)]
-        for l in range(depth):
-            layers.append(nn.Conv2d(width, width, 3, 1, 1))
-        self.layers = nn.ModuleList(layers)
-        self.policy = nn.Conv2d(width, 1, 1)
+        layers = [nn.Conv2d(2, channels, 3, 1, 1)]
+        for l in range(16):
+            layers.append(nn.Conv2d(channels, channels, 3, 1, 1))
+        layers.append(nn.Conv2d(channels, 1, 1))
 
-    def forward(self, obs, valid):
-        B, boardsize, boardsize, _ = obs.shape
-        prep = torch.cat([obs, 
-            torch.zeros((B, boardsize, boardsize, 12), device=obs.device)], -1)
-        x = prep.permute(0, 3, 1, 2)
+        self.layers = nn.ModuleList(layers)
+
+    def forward(self, obs):
+        x = obs.permute(0, 3, 1, 2)
 
         for l in self.layers:
             x = l(x)
 
-        p = self.policy(x).flatten(1)
-        p = p.where(valid, torch.full_like(p, -np.inf)).log_softmax(-1)
-
-        return p
+        return x.flatten(1).log_softmax(-1)
 
 def run():
-    batch_size = 64*1024
-    boardsize = 7
+    batch = 64*1024
+    width = 7
     device = 'cuda'
 
     network = Model().to(device)
-    opt = torch.optim.Adam(network.parameters(), lr=1e-2, amsgrad=True)
+    opt = torch.optim.Adam(network.parameters(), amsgrad=True)
     scaler = torch.cuda.amp.GradScaler()
 
+    obs = torch.zeros((batch, width, width, 2), device=device)
+    yhat = -torch.ones((batch, width**2), device=device)
+    
+    mask = torch.ones((batch,), device=device).bool()
+
     while True:
-        obs=torch.zeros((batch_size, boardsize, boardsize, 2), device=device)
-        valid=torch.randint(0, 2, (batch_size, boardsize**2), device=device).bool()
-        logits0 = -torch.ones((batch_size, boardsize**2), device=device)
-        
-        mask = torch.randint(0, 2, (batch_size,), device=device).bool()
-
         with torch.cuda.amp.autocast():
-            logits = network(obs, valid)
-
-            zeros = torch.zeros_like(logits)
-            policy_loss = -(logits0.float().exp()*logits).where(valid, zeros).sum(axis=-1)[mask].mean()
-
-            loss = policy_loss 
+            y = network(obs)
+            loss = -(yhat.exp()*y).sum(axis=-1)[mask].mean()
 
         opt.zero_grad()
         scaler.scale(loss).backward()
         scaler.step(opt)
         scaler.update()
-        
         print('Stepped')
 
 if __name__ == '__main__':
