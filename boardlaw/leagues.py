8528e6b7b3426ffaf6f328435cc9eb736eef5b45
Andrew Jones <andyjones.ed@gmail.com>
2021-01-16 11:09:21 +0000

    Adding more tests to the league

--- boardlaw/leagues.py
+++ boardlaw/leagues.py
@@ -1,26 +1,11 @@
-"""
-* Get a stream of agents
-* Get a stream of wins/losses
-* Emit which agent to play in each step
-* Use emissions and wins/losses to figure out each agent's 'responsibility' 
-  for winning/losing a game.
-* Use these responsibilities to figure out pairwise winrates
-  * Convolve responsibilities with a blurring kernel, so that similar agents
-    can have their winrates refined
-  * Will need to extend responsibilities tracker into the future, or have 
-    new agents inherit some fraction of the old agents' winrates
-* Use pairwise winrates and previous emission to figure out who to emit next
-  * Want to emit agents that'll win against the latest agent
-  * Want to emit agents that're dissimilar to recently emitted agents
-  * Want to bias strongly towards emitting the latest agent
-"""
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from logging import getLogger
 from torch import nn
 import torch.cuda
 from torch.nn import functional as F
-from rebar import arrdict, profiling
+from rebar import dotdict, arrdict, profiling
 
 log = getLogger(__name__)
 
@@ -37,7 +22,7 @@ def assemble(agentfunc, state_dict):
     new.load_state_dict(state_dict)
     return new
 
-class LeagueNetwork(nn.Module):
+class Splitter(nn.Module):
 
     def __init__(self, names, slices, field):
         super().__init__()
@@ -77,7 +62,7 @@ class LeagueNetwork(nn.Module):
     def load_state_dict(self, sd):
         self.network.load_state_dict(sd)
 
-def league_network(stable, agentfunc, n_envs, n_fielded, prime_frac):
+def splitter(stable, agentfunc, n_envs, n_fielded, prime_frac):
 
     if n_fielded:
         n_prime_envs = int(prime_frac*n_envs)
@@ -100,46 +85,48 @@ def league_network(stable, agentfunc, n_envs, n_fielded, prime_frac):
         network.load_state_dict(sd)
         field.append(network)
 
-    return LeagueNetwork(names, slices, field)
+    return Splitter(names, slices, field)
 
 class Stable:
 
-    def __init__(self, default, n_stabled, stable_interval):
+    def __init__(self, default, n_stabled, stable_interval, verbose=True):
         #TODO: This is a bit lazy; doesn't deal with games that have multiple networks playing in them,
         # and doesn't deal with networks that get evicted from the stable while they're still on the field.
         self.n_stabled = n_stabled
-
         self.stable_interval = stable_interval
+        self.verbose = verbose
 
         self.names = [-1]
-        self.stable = [default]
+        self.stable = [clone(default.state_dict())]
 
         self.losses = np.full((n_stabled,), 1)
         self.wins = np.full((n_stabled,), 1)
 
         self.step = 0
 
-    def update_stats(self, league_eval, league_seat, transition):
+    def update_stats(self, splitter, league_seat, transition):
         rewards = transition.rewards.gather(1, league_seat[:, None].long()).squeeze(-1).cpu().numpy()
-        for n, s in zip(league_eval.names, league_eval.slices):
+        for n, s in zip(splitter.names, splitter.slices):
             if n in self.names:
                 i = self.names.index(n)
                 self.wins[i] += (rewards[s] == 1).sum()
                 self.losses[i] += (rewards[s] == -1).sum()
 
     def update_stable(self, network):
+        rates = (self.wins/(self.wins + self.losses))[:len(self.stable)]
+
         if self.step % self.stable_interval == 0:
             if len(self.stable) == self.n_stabled:
-                old = np.random.randint(len(self.names))
+                old = rates.argmin()
+                self.log(f'Network #{self.step} stabled; #{self.names[old]} removed')
                 self.names[old] = self.step
                 self.stable[old] = clone(network.state_dict())
                 self.losses[old] = 0
                 self.wins[old] = 1
-                log.info(f'Network #{self.step} stabled; #{old} removed')
             else:
                 self.names.append(self.step)
                 self.stable.append(clone(network.state_dict()))
-                log.info(f'Network #{self.step} stabled')
+                self.log(f'Network #{self.step} stabled')
 
         self.step += 1
 
@@ -150,36 +137,43 @@ class Stable:
         i = np.random.choice(np.arange(len(dist)), p=dist)
         return self.names[i], self.stable[i]
 
+    def log(self, m):
+        if self.verbose:
+            log.info(m)
+
 class Field:
 
-    def __init__(self, n_fielded):
+    def __init__(self, n_fielded, verbose=True):
         self.n_fielded = n_fielded
         self.games = np.zeros((n_fielded,))
+        self.verbose = verbose
 
-    def update_stats(self, league_eval, transition):
+    def update_stats(self, splitter, transition):
         transition = transition.detach().cpu().numpy()
-        for i, s in enumerate(league_eval.slices):
+        for i, s in enumerate(splitter.slices):
             self.games[i] += transition.terminal[s].sum()
 
-    def update_field(self, league_eval, stable):
+    def update_field(self, splitter, stable):
         # Figure out who's been playing too long. Stagger it a bit so they don't all change at once
         threshold = np.linspace(1.5, 2.5, self.n_fielded)
-        for i, (n, s) in enumerate(zip(league_eval.names, league_eval.slices)):
+        for i, (n, s) in enumerate(zip(splitter.names, splitter.slices)):
             replace = self.games[i] >= threshold[i]*(s.stop - s.start)
         # Swap out any over the limit
             if replace:
                 name, sd = stable.draw()
-                league_eval.field[i].load_state_dict(sd)
+                splitter.field[i].load_state_dict(sd)
+                splitter.names[i] = name
 
-                log.info(f'New opponent is #{name}')
+                if self.verbose:
+                    log.info(f'New opponent is #{name}')
 
                 self.games[i] = 0
 
 class League:
 
     def __init__(self, agentfunc, n_envs, 
-            n_fielded=4, n_stabled=10, prime_frac=3/4, 
-            stable_interval=100, device='cuda'):
+            n_fielded=4, n_stabled=16, prime_frac=3/4, 
+            stable_interval=100, device='cuda', verbose=True):
 
         self.n_envs = n_envs
         self.n_opponents = n_fielded
@@ -187,19 +181,19 @@ class League:
         self.stable_interval = stable_interval
         self.device = device
 
-        self.stable = Stable(agentfunc().network.state_dict(), n_stabled, stable_interval)
+        self.stable = Stable(agentfunc().network, n_stabled, stable_interval, verbose=verbose)
         self.field = Field(n_fielded)
-        self.league_eval = league_network(self.stable, agentfunc, n_envs, n_fielded, prime_frac)
+        self.splitter = splitter(self.stable, agentfunc, n_envs, n_fielded, prime_frac, verbose=verbose)
 
         self.update_mask(False)
 
     def is_league(self, agent):
-        return agent.network == self.league_eval
+        return agent.network == self.splitter
 
     def update_mask(self, is_league):
         is_prime = torch.full((self.n_envs,), True, device=self.device)
         if is_league:
-            for s in self.league_eval.slices:
+            for s in self.splitter.slices:
                 is_prime[s] = False
         self.is_prime = is_prime
 
@@ -208,15 +202,15 @@ class League:
         league_stepped = self.is_league(agent)
         if league_stepped:
             league_seat = seats
-            agent.network = self.league_eval.network
+            agent.network = self.splitter.network
         else:
             league_seat = 1 - seats
-            self.league_eval.network = agent.network
-            agent.network = self.league_eval
+            self.splitter.network = agent.network
+            agent.network = self.splitter
 
-        self.stable.update_stats(self.league_eval, league_seat, transition)
-        self.field.update_stats(self.league_eval, transition)
-        self.field.update_field(self.league_eval, self.stable)
+        self.stable.update_stats(self.splitter, league_seat, transition)
+        self.field.update_stats(self.splitter, transition)
+        self.field.update_field(self.splitter, self.stable)
         self.stable.update_stable(agent.network)
 
         self.update_mask(self.is_league(agent))
@@ -231,7 +225,7 @@ class MockWorlds(arrdict.namedarrtuple(fields=('seats', 'cumulator', 'lengths'))
         return cls(
             seats=torch.full((n_envs,), 0, device=device),
             lengths=torch.full((n_envs,), 1, device=device),
-            cumulator=torch.full((n_envs,), 0., device=device))
+            cumulator=torch.full((n_envs, 2), 0., device=device))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -247,9 +241,9 @@ class MockWorlds(arrdict.namedarrtuple(fields=('seats', 'cumulator', 'lengths'))
         new_lengths = self.lengths + 1
         new_lengths[terminal] = 1
 
-        new_cumulator = self.cumulator - 2*(self.seats - .5)*skills
+        new_cumulator = self.cumulator.scatter_add(1, self.seats[:, None], skills[:, None].float())
 
-        rates = torch.sigmoid(new_cumulator/4)
+        rates = torch.sigmoid((new_cumulator[:, 0] - new_cumulator[:, 1])/4)
         rewards = terminal * (2*(torch.rand((self.n_envs,),) <= rates) - 1)
         rewards = torch.stack([rewards, -rewards], -1)
 
@@ -284,36 +278,89 @@ class MockAgent:
     def __call__(self, worlds):
         return self.network(worlds)
 
-def demo():
+def test_world():
     worlds = MockWorlds.initial(128)
+    fst = MockAgent(0)
+    snd = MockAgent(10)
 
-    league = League(MockAgent, worlds.n_envs, stable_interval=8)
+    rewards = []
+    for _ in range(8):
+        for agent in (fst, snd):
+            skills = agent(worlds)
+            worlds, transitions = worlds.step(skills)
+            rewards.append(transitions.rewards)
+    wins = (torch.stack(rewards) == 1).sum(0).sum(0)
+    assert wins[0] < wins[1]
+
+    worlds = MockWorlds.initial(128)
+    fst = MockAgent(10)
+    snd = MockAgent(0)
+
+    rewards = []
+    for _ in range(8):
+        for agent in (fst, snd):
+            skills = agent(worlds)
+            worlds, transitions = worlds.step(skills)
+            rewards.append(transitions.rewards)
+    wins = (torch.stack(rewards) == 1).sum(0).sum(0)
+    assert wins[0] > wins[1]
+
+def test_stable():
+    agent = MockAgent(0)
+    stable = Stable(agent, 2, 1, verbose=False)
+
+    splitter = Splitter([-1], [slice(0, 1)], [agent])
+
+    stable.update_stats()
+
+    pass
+
+
+def demo():
+    T = 1000
+    n_env = 128
+    n_stabled = 8
+    worlds = MockWorlds.initial(n_env)
+
+    league = League(MockAgent, worlds.n_envs, n_stabled=n_stabled, stable_interval=8, verbose=False)
 
     agent = MockAgent(0)
     network = agent.network
 
-    results = []
-    for t in range(1000):
+    fielded = np.zeros((T, T))
+    stabled = np.zeros((T, T))
+    for t in range(15):
         skills = agent(worlds)
         worlds, transitions = worlds.step(skills)
         league.update(agent, worlds.seats, transitions)
 
-        network.skill = t**.5
+        network.skill = 10. if t < 10 else 1.
 
-        names = np.zeros(worlds.n_envs)
-        for n, s in zip(league.league_eval.names, league.league_eval.slices):
-            names[s] = n
-        results.append(names)
+        for n, s in zip(league.splitter.names, league.splitter.slices):
+            fielded[t, n] = s.stop - s.start
+        
+        for n in league.stable.names:
+            stabled[t, n] = 1.
 
-    results = np.stack(results)
+    parts = dotdict.dotdict(
+        agent=agent,
+        league=league,
+        worlds=worlds)
+    trace = dotdict.dotdict(
+        fielded=fielded,
+        stabled=stabled)
 
-    return league, results
+    return parts, trace
 
-def plot_results(results):
-    import matplotlib.pyplot as plt
-    for i in range(88, 128, 8):
-        y = results[1::2, i].cpu()
-        x = np.arange(len(y))
-        plt.scatter(x, y, marker='.')
+def plot_trace(trace):
+    fig, (l, r) = plt.subplots(1, 2)
+
+    l.imshow(trace.stabled)
+    l.set_title('stabled')
+
+    r.imshow(trace.fielded)
+    r.set_title('fielded')
+
+    fig.set_size_inches(18, 9)
 
 
