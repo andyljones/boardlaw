60a033c9d0925bd0cdac229ccc8e7da856710815
Andrew Jones <andyjones.ed@gmail.com>
2021-01-18 11:04:18 +0000

    Trials different c_puct and node counts

--- experiments/mctselo.py
+++ experiments/mctselo.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import torch
 from pavlov import storage, runs
 from boardlaw import hex, mohex, mcts, analysis
@@ -9,15 +10,19 @@ def count_wins(transitions):
                 .eq(1)
                 .sum(0))
 
-def test(n_nodes):
-    run = '*great-fits'
+def test(run, snapshot=-1, **kwargs):
     boardsize = runs.info(run)['boardsize']
-    worlds = hex.Hex.initial(n_envs=256, boardsize=boardsize)
+    worlds = hex.Hex.initial(n_envs=1024, boardsize=boardsize)
+
     network = storage.load_raw(run, 'model')
-    sd = storage.load_snapshot(run, n=4)['agent']
+    sd = storage.load_snapshot(run, n=snapshot)['agent']
     network.load_state_dict(storage.expand(sd)['network'])
-    A = mcts.MCTSAgent(network, n_nodes=n_nodes)
-    B = mcts.DummyAgent(network)
+    A = mcts.MCTSAgent(network.cuda(), **kwargs)
+
+    network = storage.load_raw(run, 'model')
+    sd = storage.load_snapshot(run, n=snapshot)['agent']
+    network.load_state_dict(storage.expand(sd)['network'])
+    B = mcts.DummyAgent(network.cuda())
 
     fst = analysis.rollout(worlds, [A, B], n_reps=1, eval=False)
     snd = analysis.rollout(worlds, [B, A], n_reps=1, eval=False)
@@ -27,3 +32,16 @@ def test(n_nodes):
     rate = wins[0]/wins.sum()
     elo = torch.log(rate) - torch.log(1 - rate)
     return elo.item()
+
+def run():
+    run = '*great-fits'
+    results = []
+    for c in [1/32, 1/16, 1/8, 1/4, 1/2, 1, 2]:
+        for n in [2, 4, 8, 16, 32, 64, 128, 256]:
+            results.append({
+                'c_puct': c,
+                'n_nodes': n,
+                'elo': test(run, -1, c_puct=c, n_nodes=n)})
+            print(results[-1])
+    df = pd.DataFrame(results).pivot('c_puct', 'n_nodes', 'elo')
+    return df
