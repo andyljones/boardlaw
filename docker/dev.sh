768bcfab10c593b639505b7c95cdb3de41730adf
Andrew Jones <andyjones.ed@gmail.com>
2021-01-20 16:32:36 +0000

    Sets up docs

--- docker/dev.sh
+++ docker/dev.sh
@@ -4,16 +4,16 @@ echo "Setting up SSH server"
 env | egrep -v "^(HOME=|USER=|MAIL=|LC_ALL=|LS_COLORS=|LANG=|HOSTNAME=|PWD=|TERM=|SHLVL=|LANGUAGE=|_=)" >> /etc/environment
 # service ssh start
 
-# echo "Launching MPS"
-# nvidia-cuda-mps-control -d
+echo "Launching MPS"
+nvidia-cuda-mps-control -d
 
 echo "Launching Jupyter"
 mkdir -p output/logs
 nohup jupyter notebook --notebook-dir . --no-browser --port=5000 --ip=0.0.0.0 --allow-root --NotebookApp.token="" --NotebookApp.password="" >output/logs/jupyter.log 2>&1 &
 
-# echo "Launching docs server"
-# mkdir -p docs/_build
-# nohup python -m http.server --directory docs/_build 9095 >output/logs/docserver.log 2>&1 &
+echo "Launching docs server"
+mkdir -p docs/_build
+nohup python -m http.server --directory docs/_build 9095 >output/logs/docserver.log 2>&1 &
 
 echo "Entering loop"
 while sleep 1000; do :; done
