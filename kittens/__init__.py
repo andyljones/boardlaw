3711764db44e0eca294ee6d91e1e56afece02485
Andrew Jones <andyjones.ed@gmail.com>
2021-01-22 16:39:25 +0000

    Starts on kittens

---
+++ kittens/__init__.py
@@ -0,0 +1,12 @@
+"""A very simple remote job manager. So named because running remote jobs is like herding kittens.
+
+* Experiments are gonna be defined as an archive file with all the code in, plus a command to run.
+* Keep track of what's run and what's running in a central json file. This way it's idempotent!
+    * Keep track of the machine ID and the PID in particular.
+* Generate as many copies of the archive file as you need, each one with its own config file.
+* Use Hydra or Dynaconf or something to load the config in. Or just do it myself.
+
+""" 
+
+import fabric
+
