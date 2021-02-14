5885fe7c7d96f64c8454635e4237e35c185280a3
Andrew Jones <andyjones.ed@gmail.com>
2021-02-14 14:35:55 +0000

    Gets SSH connections to the container working

--- grid/gcp.py
+++ grid/gcp.py
@@ -28,11 +28,10 @@ def instances():
     instances = api().instances().list(project=PROJECT, zone=ZONE).execute().get('items', [])
     return {i['name']: i for i in instances}
 
-def connection(name):
+def host_connection(name):
     # gcloud beta compute ssh --zone "us-west1-a" "andyjones_ed@boardlaw-1" --project "andyljones" --container "klt-boardlaw-refine-1-ncbb"
     # Fixing container name doesn't seem possible atm: https://stackoverflow.com/questions/55998957/how-to-specify-the-container-name-running-on-google-compute-engine
     # Fix SSH user name
-    pass
     info = instances()[name]
     external_ip = info['networkInterfaces'][0]['accessConfigs'][0]['natIP']
     return Connection( 
@@ -44,9 +43,15 @@ def connection(name):
             'look_for_keys': False, 
             'key_filename': ['/root/.ssh/google_compute_engine']})
 
-def run(name, command):
-    conn = connection(name)
-    container = conn.run('docker ps -aqf "ancestor=andyljones/boardlaw"', hide=True).stdout.strip()
-    conn.run(f'docker exec {container} {command}')
-
-# localhost ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDBpbJwqfPafs6Ve8Q9gLvxPUURFKY6bbxLGCWX1VtRLo8DAfzN4BF6OYdwCwGSEmoUB/fUXz+UGnQQkVpcnyMJXP2SJEYPu735/HH6n7AjMIQFX9zEMRcTly7KpT/8sTOckV/Nh0D0ADIiPMbLTXFjCqg6o842TbC3R/FH7aYBFt055INcEBclrIdnSZM2GymDsoG7F2JUsqWNlZs37xnw+PBd/IdF6hdgQnVMWvyCGTjvA1j386hwhJcctgaOk6lkNqMD9hTjuW5BxBGwKrWqgwbXyrFybPYVyrShoXNIEov7oMLaGhK5e99T5KH9ZfxuJXV6edF+kwgJa1FzVtHJqwGKXN06CqhC9+/ItIeRhTqvW/5BDUloUsDFi6l+hzJRzGR0mUndnYCKcSdOT1E/tHQ0mnydP2CM5U2xXQWaq2ZaXxINN5ZqqH1W3E0ozra8muAlAgbXCFb7PMAg44DopSGr6s7UIb957wtuh6fc0pFSfqWRKvxn0Two23+r0hU=
+def container_connection(name):
+    info = instances()[name]
+    external_ip = info['networkInterfaces'][0]['accessConfigs'][0]['natIP']
+    return Connection( 
+        host=external_ip, 
+        user='root', 
+        # Make sure to add a firewall rule permitting 36022
+        port=36022, 
+        connect_kwargs={
+            'allow_agent': False, 
+            'look_for_keys': False, 
+            'key_filename': ['/root/.ssh/boardlaw_rsa']})
