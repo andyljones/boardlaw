294c31141771ef387cbbd20d899a5722c63e5e4a
Andrew Jones <andyjones.ed@gmail.com>
2021-02-18 10:31:35 +0000

    Checkpoint before I try and parallelize this

--- grid/eval.py
+++ grid/eval.py
@@ -4,6 +4,7 @@ import pandas as pd
 from boardlaw import arena
 from pavlov import storage, runs
 from rebar import dotdict
+from IPython import display
 
 def snapshots(boardsize):
     snapshots = {}
@@ -25,6 +26,23 @@ def worlds_and_agents(boardsize):
     
     return worlds, agents
 
+def update(games, wins, results):
+    games, wins = games.copy(), wins.copy()
+    for result in results:
+        games.loc[result.names[0], result.names[1]] += result.games
+        games.loc[result.names[1], result.names[0]] += result.games
+        wins.loc[result.names[0], result.names[1]] += result.wins[0]
+        wins.loc[result.names[1], result.names[0]] += result.wins[1]
+    return games, wins
+
+def report(soln):
+    μ, σ = arena.analysis.difference(soln, soln.μ.idxmin())
+
+    display.clear_output(wait=True)
+    print(f'nonzero: {(soln.μ != 0).mean():.0%}')
+    print(f'μ_max: {μ.max():.1f}')
+    print(f'σ_ms: {σ.pow(2).mean()**.5:.2f}')
+
 def run(boardsize=3):
     worlds, agents = worlds_and_agents(boardsize)
 
@@ -32,10 +50,12 @@ def run(boardsize=3):
     wins  = pd.DataFrame(np.zeros((n_agents, n_agents)), list(agents), list(agents))
     games = pd.DataFrame(np.zeros((n_agents, n_agents)), list(agents), list(agents))
 
-    soln = activelo.solve(games, wins)
-    sugg = activelo.suggest(soln)
+    soln = None
+    while True:
+        soln = activelo.solve(games, wins, soln=soln)
+        sugg = activelo.suggest(soln)
 
-    results = arena.common.evaluate(worlds, {n: agents[n] for n in sugg})
-    pass
-    # for runs 
+        results = arena.common.evaluate(worlds, {n: agents[n] for n in sugg})
+        games, wins = update(games, wins, results)
 
+        report(soln)
