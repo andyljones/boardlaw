a987023ab93fc70680236bf5fef3f795a4f40666
Andrew Jones <andyjones.ed@gmail.com>
2021-02-24 09:45:04 +0000

    Adds model to frontier plot

--- grid/plot.py
+++ grid/plot.py
@@ -1,3 +1,4 @@
+import statsmodels.formula.api as smf
 import pandas as pd
 import numpy as np
 import plotnine as pn
@@ -62,18 +63,40 @@ def _interp_frontier(g, var):
     return ys.max(1).pipe(lambda s: s[s.expanding().max() == s]).rename_axis(index='flops').rename('μ')
 
 def plot_interp_frontier(snaps):
+    cutoffs = {
+        3: (6e10, 2e11),
+        4: (2e11, 2e12),
+        5: (3e11, 2e13),
+        6: (7e11, 3e14),
+        7: (1e12, 3e15),
+        8: (1e12, 1e16),
+        9: (1e12, 1e17)}
+    cutoffs = pd.DataFrame.from_dict(cutoffs, orient='index', columns=('lower', 'upper')).rename_axis(index='boardsize')
+
     df = (snaps
+            .join(cutoffs, on='boardsize')
+            .query('lower <= flops <= upper')
             .groupby('boardsize')
             .apply(_interp_frontier, 'flops')
             .reset_index())
-    return (pn.ggplot(df, pn.aes(x='flops', y='400/np.log(10)*μ', color='factor(boardsize)', group='boardsize'))
-            + pn.geom_line(size=2)
+
+    model = smf.ols('μ ~ boardsize + np.log10(flops) + 1', df).fit()
+    df['μhat'] = model.predict(df)
+
+    ps = model.params.mul(400/np.log(10)).apply(lambda x: f'{float(f"{x:.1g}"):g}')
+    s = f'elo = {ps.boardsize}·boardsize + {ps["np.log10(flops)"]}·log10(flops) + C'
+
+    (pn.ggplot(df, pn.aes(x='flops', color='factor(boardsize)', group='boardsize'))
+            + pn.geom_line(pn.aes(y='400/np.log(10)*μ'), size=2)
+            + pn.geom_line(pn.aes(y='400/np.log(10)*μhat'), size=1, linetype='dashed')
+            + pn.annotate('text', 5e14, -2500, label=s, size=20)
             + pn.labs(
                 x='training flops', 
                 y='elo v. perfect play',
                 title='performance frontier in terms of compute')
             + pn.scale_x_continuous(trans='log10')
             + pn.scale_color_discrete(name='boardsize')
+            + pn.coord_cartesian(None, (None, 0))
             + mpl_theme()
             + poster_sizes())
 
