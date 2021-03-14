a69178858a2a65b6ef02f3d5df86e7d799f79fdb
Andrew Jones <andyjones.ed@gmail.com>
2021-03-14 15:29:47 +0000

    Adds changepoint model

--- experiments/talk.py
+++ experiments/talk.py
@@ -1,3 +1,5 @@
+import torch
+from torch import nn
 import aljpy
 import hashlib
 import json
@@ -82,13 +84,49 @@ def interp_frontier(g, x='train_flops', y='elo', group='run'):
         ys[run] = np.interp(xs, xp, yp, np.nan, np.nan)
     ys = pd.DataFrame(ys, index=10**xs)
 
-    return ys.max(1).pipe(lambda s: s[s.expanding().max() == s]).rename_axis(index=x).rename(y)
+    return ys.max(1).rename_axis(index=x).rename(y)
 
+class Changepoint(nn.Module):
+
+    def __init__(self, D=2):
+        super().__init__()
+        self.lower = nn.Parameter(torch.as_tensor([-1.5, 3.]))
+        self.linear = nn.Parameter(torch.as_tensor([2., -2, -16]))
+        
+    def forward(self, X):
+        X = torch.cat([X, torch.ones_like(X[:, :1])], -1)
+        lower = X[:, 1:] @ self.lower
+        linear = X @ self.linear
+        return torch.maximum(linear, lower).clamp(None, 0)
+
+def fit_changepoint(df):
+    X = torch.stack([
+        torch.as_tensor(df.train_flops).log10().float(),
+        torch.as_tensor(df.boardsize).float(),], -1)
+
+    y = torch.as_tensor(df.elo)
+
+    model = Changepoint()
+    optim = torch.optim.LBFGS(model.parameters(), line_search_fn='strong_wolfe', max_iter=100)
+
+    def closure():
+        yhat = model(X)
+        loss = (y - yhat).pow(2).mean()
+        optim.zero_grad()
+        loss.backward()
+        return loss
+        
+    optim.step(closure)
+
+    return pd.Series(model(X).detach().cpu().numpy(), df.index)
+        
 def plot_flops_frontier(ags):
     df = (ags.query('test_nodes == 64')
             .groupby('boardsize')
             .apply(interp_frontier, 'train_flops')
             .reset_index()) 
+    
+    df['elohat'] = fit_changepoint(df)
 
     return (pn.ggplot(df, pn.aes(x='train_flops', color='factor(boardsize)', group='boardsize'))
                 + pn.geom_line(pn.aes(y='400/np.log(10)*elo'), size=2)
