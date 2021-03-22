b31837af18995e0173ca37d755e83f206c91c4d7
Andrew Jones <andyjones.ed@gmail.com>
2021-03-22 16:56:15 +0000

    More IEEE theming. Some serious bikeshedding here

--- analysis/paper.py
+++ analysis/paper.py
@@ -7,14 +7,14 @@ def plot_frontiers(ags):
     labels = df.sort_values('train_flops').groupby('boardsize').first().reset_index()
 
     return (pn.ggplot(df, pn.aes(x='train_flops/(1e12*3600)', color='factor(boardsize)', group='boardsize'))
-                + pn.geom_line(pn.aes(y='400/np.log(10)*elo'), size=.5, show_legend=False)
-                + pn.geom_line(pn.aes(y='400/np.log(10)*elohat'), size=.25, linetype='dashed', show_legend=False)
+                + pn.geom_line(pn.aes(y='400/np.log(10)*elo'), size=1, show_legend=False)
+                + pn.geom_line(pn.aes(y='400/np.log(10)*elohat'), size=.5, linetype='dashed', show_legend=False)
                 + pn.geom_text(pn.aes(y='400/np.log(10)*elohat', label='boardsize'), data=labels, show_legend=False, size=8, nudge_x=-.25, nudge_y=-25)
                 + pn.labs(
                     x='GPU-hours', 
                     y='Elo v. perfect play')
                 + pn.scale_x_continuous(trans='log10')
                 + pn.coord_cartesian(None, (None, 0))
-                + plot.ieee())
+                + plot.IEEE())
 
 
--- analysis/plot.py
+++ analysis/plot.py
@@ -15,11 +15,24 @@ def poster_sizes():
         title=pn.element_text(size=18),
         legend_title=pn.element_text(size=18))
 
+class IEEE(pn.theme):
+
+    def __init__(self):
+        super().__init__(complete=True)
+
+        self._rcParams.update({
+            'figure.figsize': (5, 4),
+            'font.family': 'serif',
+            'font.size': 8,
+            'axes.grid': True,
+            'axes.grid.which': 'both',
+            'grid.alpha': .2})
+        
 
 def ieee():
     # https://github.com/garrettj403/SciencePlots/blob/master/styles/journals/ieee.mplstyle
     return [
-        pn.theme_matplotlib(),
+        pn.theme_mpl(),
         pn.theme(
             strip_background=pn.element_rect(color='w', fill='w'),
             panel_grid=pn.element_line(color='k', alpha=.1, size=.25),
@@ -27,7 +40,7 @@ def ieee():
             text=pn.element_text(family='serif', size=8),
             legend_title=pn.element_text(size=8),
             figure_size=(3.3, 2.5), 
-            dpi=240,)]
+            dpi=180,)]
 
 def no_colorbar_ticks():
     return pn.guides(color=pn.guide_colorbar(ticks=False))
