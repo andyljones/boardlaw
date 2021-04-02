Fixes an overflow in the Elos solver

analysis/data.py
@@ -15,21 +15,25 @@ from boardlaw import arena
 ELO = 400/np.log(10)
 
 @aljpy.autocache()
-def _trial_elos(boardsize, counter):
+def _trial_elos(boardsize, counter, main_only):
     trials = sql.trial_query(boardsize, 'bee/%')
     ws, gs = elos.symmetrize(trials)
+    if main_only:
+        ags = sql.agent_query().query('test_nodes == 64')
+        mask = ws.index.isin(ags.index)
+        ws, gs = ws.loc[mask, mask], gs.loc[mask, mask]
     return elos.solve(ws, gs)
 
-def trial_elos(boardsize):
+def trial_elos(boardsize, main_only=False):
     counter = sql.file_change_counter()
-    return _trial_elos(boardsize, counter)
+    return _trial_elos(boardsize, counter, main_only)
 
-def load():
+def load(main_only=False):
     ags = sql.agent_query()
 
     es = []
     for b in tqdm(ags.boardsize.unique()):
-        es.append(trial_elos(b))
+        es.append(trial_elos(b, main_only))
     es = pd.concat(es)
 
     return ags.join(es, how='inner')

boardlaw/elos.py
@@ -25,19 +25,22 @@ def solve(wins, games, prior=1):
     pd.testing.assert_index_equal(wins.columns, games.columns)
     pd.testing.assert_index_equal(wins.index, wins.columns, check_names=False)
 
-    W = torch.as_tensor(wins.fillna(0).values) + prior
-    N = torch.as_tensor(games.fillna(0).values) + 2*prior
+    W = torch.as_tensor(wins.fillna(0).values).double() + prior
+    N = torch.as_tensor(games.fillna(0).values).double() + 2*prior
     mask = torch.as_tensor(games.gt(0).values)
 
     n = N.shape[0]
-    r = torch.nn.Parameter(torch.zeros(n))
+    r = torch.nn.Parameter(torch.zeros(n).double())
 
     def loss():
         d = r[:, None] - r[None, :]
         s = 1/(1 + torch.exp(-d))
         
         l = W*s.log() + (N - W)*(1 - s).log()
-        return -l[mask].mean() + .01*r.sum().pow(2)
+        loss = -l[mask].mean() + .01*r.mean().pow(2)
+        if torch.isinf(loss):
+            breakpoint()
+        return loss
 
     optim = torch.optim.LBFGS([r], line_search_fn='strong_wolfe', max_iter=200)
 
@@ -47,6 +50,7 @@ def solve(wins, games, prior=1):
         l.backward()
         return l
         
+    optim.step(closure)
     optim.step(closure)
     closure()
 